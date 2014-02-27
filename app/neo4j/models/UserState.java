c3e419e4b98710ccffbd54f45702cc853fc56e7b
Johannes Unterstein <unterstein@me.com>
2014-02-27 07:19:16 +0100

    removed unused class

--- app/neo4j/models/UserState.java
+++
@@ -1,9 +0,0 @@
-package neo4j.models;
-
-import java.io.Serializable;
-
-public enum UserState implements Serializable {
-  ACTIVE, // everything is fine
-  LOCK // user are not allowed to lock in
-  ;
-}
