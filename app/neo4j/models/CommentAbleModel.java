de5c0cefd7c33bdc6fb7c3d910bcf426efaff38a
Johannes Unterstein <unterstein@me.com>
2014-02-27 07:04:24 +0100

    added annotation

--- app/neo4j/models/CommentAbleModel.java
+++ app/neo4j/models/CommentAbleModel.java
@@ -2,10 +2,12 @@ package neo4j.models;
 
 import neo4j.relations.Relations;
 import org.neo4j.graphdb.Direction;
+import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
 
 import java.util.Set;
 
+@NodeEntity
 public class CommentAbleModel extends AbstractAuthoredModel {
 
   @RelatedTo(type = Relations.COMMENTABLE_COMMENT, direction = Direction.INCOMING)
