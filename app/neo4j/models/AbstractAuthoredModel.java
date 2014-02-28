7512d7afa4dff145a2d208f01c51e3db2f17ab5b
Johannes Unterstein <unterstein@me.com>
2014-02-28 08:39:12 +0100

    changed direction

--- app/neo4j/models/AbstractAuthoredModel.java
+++ app/neo4j/models/AbstractAuthoredModel.java
@@ -2,12 +2,13 @@ package neo4j.models;
 
 import neo4j.models.user.User;
 import neo4j.relations.Relations;
+import org.neo4j.graphdb.Direction;
 import org.springframework.data.neo4j.annotation.NodeEntity;
 import org.springframework.data.neo4j.annotation.RelatedTo;
 
 @NodeEntity
 public class AbstractAuthoredModel extends AbstractModel {
 
-  @RelatedTo(type = Relations.MODEL_AUTHOR)
+  @RelatedTo(type = Relations.MODEL_AUTHOR, direction = Direction.INCOMING)
   public User author;
 }
